5a86fc49af1b53a4ba5a2a620468956452bace33
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-03-05 11:17:31 -0300

    Fixes permission checking on ImoutoAttribute

--- Attributes/ImoutoAttribute.cs
+++ Attributes/ImoutoAttribute.cs
@@ -1,12 +1,13 @@
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace sisbase.Attributes
 {
 	public class ImoutoAttribute : CheckBaseAttribute
 	{
-		public override async Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help) => ctx.Member.PermissionsIn(ctx.Channel).HasPermission(Permissions.ManageRoles);
+		public override async Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help) => ctx.Member.Roles.Any(x => x.Permissions.HasPermission(Permissions.ManageRoles));
 	}
 }
