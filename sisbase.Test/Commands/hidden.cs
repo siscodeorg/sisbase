680b5848ed4c1e2157108b7786bd2d1ce04d9bf7
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-03-07 01:13:53 -0300

    Added example for hidden commands

---
+++ sisbase.Test/Commands/hidden.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using sisbase.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisbase.Test.Commands
+{
+	/// <summary>
+	/// Hidden commands are hidden from the help command (unless if -h is added to the command)
+	/// </summary>
+	public class hidden : BaseCommandModule
+	{
+		[Command("hidden")]
+		//To make a command hidden you only need to add this attribute.
+		[Hidden]
+		public async Task hiddenCommand(CommandContext ctx) => await ctx.RespondAsync(embed: EmbedBase.OutputEmbed("I'm a sneaky snek."));
+	}
+}
