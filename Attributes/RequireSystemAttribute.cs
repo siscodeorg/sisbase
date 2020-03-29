bfb3c01326cd32faed23b3821f90fa2e7d626f87
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-03-29 16:30:35 -0300

    Added RequireSystemAttribute

---
+++ Attributes/RequireSystemAttribute.cs
@@ -0,0 +1,26 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using sisbase.Utils;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sisbase.Attributes
+{
+	public class RequireSystemAttribute : CheckBaseAttribute
+	{
+		public ISystem System;
+
+		public RequireSystemAttribute(Type t)
+		{
+			if (t.GetInterfaces().Any(x => x == typeof(ISystem)))
+			{
+				SMC.RegisteredSystems.TryGetValue(t, out System);
+			}
+		}
+
+#pragma warning disable CS1998
+
+		public override async Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help) => System != null;
+	}
+}
