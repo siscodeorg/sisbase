1d5056c62279f2cdffd2197707e03152ab3bd3e1
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-04-02 21:47:39 -0300

    Add example files

---
+++ sisbase.Test/Systems/TimedSystem.cs
@@ -0,0 +1,36 @@
+﻿using sisbase.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sisbase.Test.Systems
+{
+	public class TimedSystem : ISystem, ISchedule
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public bool Status { get; set; }
+
+		public TimeSpan Timeout { get; private set; }
+
+		public void Activate()
+		{
+			Name = "TimedSystem";
+			Description = "A dummy System to test the scheduler";
+			Status = true;
+			Timeout = TimeSpan.FromMinutes(2);
+		}
+
+		public void Deactivate()
+		{
+			Name = null;
+			Description = null;
+			Status = false;
+			Timeout = TimeSpan.Zero;
+		}
+
+		public void Execute() => Logger.Log(this, "Command Executed from Execute()");
+
+		public Action RunContinuous() => new Action(() => Logger.Log(this, "Command Executed from RunContinuous()"));
+	}
+}
