52faf3e9c19cd0043c80b46cc95c72dfa00322b2
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-04-08 15:43:03 -0300

    Adds the testbed for testing random d#+ calls

---
+++ sisbase.Test/Commands/testbed.cs
@@ -0,0 +1,23 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisbase.Test.Commands
+{
+	[Group("test"), Description("The testbed commands")]
+	public class testbed : BaseCommandModule
+	{
+		[Command("gnmdm")]
+		public async Task GetNextMessageDm(CommandContext ctx)
+		{
+			var dmc = await ctx.Member.CreateDmChannelAsync();
+			var msg = await dmc.SendMessageAsync("nmdm : WAITING");
+			var response = await dmc.GetNextMessageAsync(ctx.Member);
+			await msg.ModifyAsync($"ndmm : \n{response.Result.Content}");
+		}
+	}
+}
