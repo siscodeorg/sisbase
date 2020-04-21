9d93e11c7e81d14841bd9d38dec155ff01102186
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-04-20 22:39:01 -0300

    Adds FirstEmoji extension method

--- Utils/General.cs
+++ Utils/General.cs
@@ -1,7 +1,9 @@
-﻿using sisbase.Configuration;
+﻿using DSharpPlus.Entities;
+using sisbase.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace sisbase.Utils
 {
@@ -92,5 +94,19 @@ namespace sisbase.Utils
 		/// <returns></returns>
 		public static int FirstInt(this DiscordMessage m) =>
 			int.Parse(m.Content.Split(" ").Where(x => int.TryParse(x, out int _)).FirstOrDefault());
+
+		/// <summary>
+		/// Returns the first emoji found on a <see cref="DiscordMessage"/>
+		/// </summary>
+		/// <param name="m">The message</param>
+		/// <returns></returns>
+		public static DiscordEmoji FirstEmoji(this DiscordMessage m)
+		{
+			string str = m.Content.Split(" ").Where(x => Emoji.EmojiLiterals.Contains(x) || Regex.IsMatch(x, @"\<\:([a-zA-Z_0-9]+)\:([0-9]+)\>|\<\:([a-zA-Z_0-9]+)\:\>")).FirstOrDefault();
+			if (string.IsNullOrEmpty(str)) return null;
+			if (Emoji.EmojiLiterals.Contains(str)) return DiscordEmoji.FromUnicode(str);
+			string customEmoji = Regex.Match(str, @"\:([a-zA-Z_0-9]+)\:").Value;
+			return DiscordEmoji.FromName(SisbaseBot.Instance.Client, customEmoji);
+		}
 	}
 }
