43e4348366ecec9a765bbbea80424f05bf8bb673
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-04-20 23:52:10 -0300

    Simplifies the example bot's main file

--- sisbase.Test/Program.cs
+++ sisbase.Test/Program.cs
@@ -1,34 +1,14 @@
-﻿using sisbase.Configuration;
-using sisbase.Utils;
-using System;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace sisbase.Test
 {
 	internal class Program
 	{
-		private static CancellationTokenSource Cts { get; } = new CancellationTokenSource();
-
 		private static async Task Main()
 		{
-			Console.CancelKeyPress += (sender, e) =>
-			{
-				if (!Cts.IsCancellationRequested)
-					Cts.Cancel();
-				e.Cancel = true;
-			};
-			var config = new Sisbase(Directory.GetCurrentDirectory());
-
-			config.AddCustomConfiguration<Json>("another config", new Json());
-			var sisbase = new SisbaseBot(
-				config
-			);
+			var sisbase = new SisbaseBot();
 			sisbase.RegisterBot(typeof(Program).Assembly);
-			await sisbase.StartAsync();
-			while (!Cts.IsCancellationRequested)
-				await Task.Delay(1);
+			await sisbase.Start();
 		}
 	}
 }
