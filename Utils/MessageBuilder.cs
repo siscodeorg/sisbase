85808f49478e2fe154347bed79f7539e5db1f2f9
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-04-23 19:44:52 -0300

    Adds new MessageBuilder

---
+++ Utils/MessageBuilder.cs
@@ -0,0 +1,28 @@
+﻿using DSharpPlus.Entities;
+using System.Threading.Tasks;
+
+namespace sisbase.Utils
+{
+	public class MessageBuilder
+	{
+		public DiscordEmbed Embed { get; internal set; }
+		public string Content { get; internal set; }
+
+		public MessageBuilder WithEmbed(DiscordEmbed embed)
+		{
+			Embed = embed; return this;
+		}
+
+		public MessageBuilder ClearEmbeds()
+		{
+			Embed = null; return this;
+		}
+
+		public MessageBuilder WithContent(string content)
+		{
+			Content = content; return this;
+		}
+
+		public async Task<DiscordMessage> Build(DiscordChannel channel, bool tts = false) => await channel.SendMessageAsync(Content, tts, Embed);
+	}
+}
