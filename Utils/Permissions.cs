5437bc57ea48bd97870673a89ff59d1719f66fe6
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-04-26 16:39:49 -0300

    Adds documentation

--- Utils/Permissions.cs
+++ Utils/Permissions.cs
@@ -12,7 +12,17 @@ namespace sisbase.Utils
 		public const DPerm AdministrativePermissions = DPerm.KickMembers | DPerm.BanMembers | DPerm.Administrator | DPerm.ManageChannels
 				| DPerm.ManageGuild | DPerm.ManageMessages | DPerm.ManageRoles
 				| DPerm.ManageWebhooks;
+		/// <summary>
+		/// Gets all the global permissions of a <see cref="DiscordMember"/> including <seealso cref="DiscordGuild.EveryoneRole"/>
+		/// </summary>
+		/// <param name="member">The member of the </param>
+		/// <returns></returns>
 		public static DPerm GetPermissions(this DiscordMember member) => member.Roles.Select(x => x.Permissions).Aggregate(member.Guild.EveryoneRole.Permissions,(a, b) => a | b);
+		/// <summary>
+		/// Checks if a <see cref="DiscordMember"/> has any permission from <see cref="AdministrativePermissions"/>
+		/// </summary>
+		/// <param name="member"></param>
+		/// <returns></returns>
 		public static bool IsModerator(this DiscordMember member) => (member.GetPermissions() & AdministrativePermissions) != DPerm.None;
 	}
 }
