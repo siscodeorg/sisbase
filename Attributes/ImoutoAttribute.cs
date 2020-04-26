45226c48c7e2d1fe42c186e00fc7cb32e3a9da65
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-04-26 17:01:18 -0300

    Fixes ImoutoAttribute checking. Closes #8

--- Attributes/ImoutoAttribute.cs
+++ Attributes/ImoutoAttribute.cs
@@ -1,6 +1,7 @@
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using sisbase.Utils;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,7 +14,7 @@ namespace sisbase.Attributes
 	{
 #pragma warning disable CS1998, CS1591
 
-		public override async Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help) => ctx.Member.Roles.Any(x => x.Permissions.HasPermission(Permissions.ManageRoles));
+		public override async Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help) => ctx.Member.IsModerator();
 
 #pragma warning restore CS1998
 	}
