ff651ba5da70ad0a2dbd2261f3da23d42f484be0
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-05-01 18:10:17 -0300

    Adds the Interaction Manager Controller

---
+++ Interactivity/IMC.cs
@@ -0,0 +1,24 @@
+﻿using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sisbase.Interactivity
+{
+	/// <summary>
+	/// Interaction Manager Controller
+	/// </summary>
+	internal static class IMC 
+	{
+		static IMC() => InteractionRegistry = new List<Interaction>();
+		internal static List<Interaction> InteractionRegistry { get; } = new List<Interaction>();
+		internal static void AddInteraction(Interaction interaction)
+			=> InteractionRegistry.Add(interaction);
+		internal static void RemoveIntraction(Interaction interaction)
+			=> InteractionRegistry.Remove(interaction);
+		internal static Interaction GetInteraction(DiscordMessage origin)
+			=> InteractionRegistry.Find(x => x.Origin == origin);
+		internal static void UpdateInteraction(DiscordMessage origin, Interaction delta)
+		{ InteractionRegistry.Remove(GetInteraction(origin)); AddInteraction(delta); }
+	}
+}
