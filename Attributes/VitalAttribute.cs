b1850559578f09604b51cfc487bf56e3c844b9a6
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-05-09 21:39:31 -0300

    Creates the vital attribute

---
+++ Attributes/VitalAttribute.cs
@@ -0,0 +1,14 @@
+﻿using sisbase.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace sisbase.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class VitalAttribute : Attribute
+    {
+
+    }
+}
