4a0f396916355d2421895cf7b009f3da0444bd1b
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-05-14 13:46:39 -0300

    Adds MessageUpdatedEventArgs

---
+++ Interactivity/EventArgs/MessageUpdatedEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sisbase.Interactivity.EventArgs
+{
+	public class MessageUpdatedEventArgs : DiscordEventArgs
+	{
+		public InteractionMessage After { get; internal set; }
+		public InteractionMessage Before { get; internal set; }
+		public DiscordChannel Channel
+			=> After.Channel;
+		public DiscordGuild Guild
+			=> Channel.Guild;
+		public DiscordUser Author
+			=> After.Author;
+		public IReadOnlyList<DiscordUser> MentionedUsers { get; internal set; }
+		public IReadOnlyList<DiscordRole> MentionedRoles { get; internal set; }
+		public IReadOnlyList<DiscordChannel> MentionedChannels { get; internal set; }
+		internal MessageUpdatedEventArgs(DiscordClient client) : base (client){ }
+	}
+}
