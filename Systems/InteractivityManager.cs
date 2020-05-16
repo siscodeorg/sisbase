811dd73c37b4ce83224680cec33decff676470a9
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-05-16 18:44:43 -0300

    Updates InteractivityManager to serve new Interactivity api

--- Systems/InteractivityManager.cs
+++ Systems/InteractivityManager.cs
@@ -3,72 +3,60 @@ using DSharpPlus.EventArgs;
 using sisbase.Interactivity;
 using sisbase.Utils;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace sisbase.Systems
-{
-	public class InteractivityManager : IClientSystem
-	{
-		public string Name { get;set; }
+namespace sisbase.Systems {
+	public class InteractivityManager : IClientSystem {
+		public string Name { get; set; }
 		public string Description { get; set; }
-		public bool Status { get;set;}
+		public bool Status { get; set; }
 
-		public void Activate()
-		{
+		public void Activate() {
 			Name = "InteractivityManager";
 			Description = "Dispatches all Sisbase.Interactivity events.";
 			Status = true;
 		}
-		public void ApplyToClient(DiscordClient client)
-		{
+		public void ApplyToClient(DiscordClient client) {
 			client.MessageUpdated += EditHandler;
 			client.MessageReactionAdded += ReactionAddHandler;
 			client.MessageReactionRemoved += ReactionRemovedHandler;
 			client.MessageDeleted += DeleteHandler;
 		}
 
-		private async Task ReactionRemovedHandler(MessageReactionRemoveEventArgs e)
-		{
-			var interaction = IMC.InteractionRegistry.Find(x => x.UserMessages.Contains(e.Message));
-			interaction ??= IMC.InteractionRegistry.Find(x => x.BotMessages.Contains(e.Message));
-			if (interaction == null) return;
-			await interaction.Dispatch(e);
+		private async Task ReactionRemovedHandler(MessageReactionRemoveEventArgs e) {
+			foreach (var intr in IMC.InteractionRegistry) {
+				await intr.BotMessages.Wants(e);
+				await intr.UserMessages.Wants(e);
+			}
 		}
 
-		private async Task DeleteHandler(MessageDeleteEventArgs e)
-		{
-			var interaction = IMC.InteractionRegistry.Find(x => x.UserMessages.Contains(e.Message));
-			interaction ??= IMC.InteractionRegistry.Find(x => x.BotMessages.Contains(e.Message));
-			if (interaction == null) return;
-			await interaction.Dispatch(e);
+		private async Task DeleteHandler(MessageDeleteEventArgs e) {
+			foreach (var intr in IMC.InteractionRegistry) {
+				await intr.BotMessages.Wants(e);
+				await intr.UserMessages.Wants(e);
+			}
 		}
 
-		private async Task ReactionAddHandler(MessageReactionAddEventArgs e)
-		{
-			var interaction = IMC.InteractionRegistry.Find(x => x.UserMessages.Contains(e.Message));
-			interaction ??= IMC.InteractionRegistry.Find(x => x.BotMessages.Contains(e.Message));
-			if (interaction == null) return;
-			await interaction.Dispatch(e);
+		private async Task ReactionAddHandler(MessageReactionAddEventArgs e) {
+			foreach (var intr in IMC.InteractionRegistry) {
+				await intr.BotMessages.Wants(e);
+				await intr.UserMessages.Wants(e);
+			}
 		}
-		private async Task EditHandler(MessageUpdateEventArgs e)
-		{
-			var interaction = IMC.InteractionRegistry.Find(x => x.UserMessages.Contains(e.Message));
-			interaction ??= IMC.InteractionRegistry.Find(x => x.BotMessages.Contains(e.Message));
-			if (interaction == null) return;
-			await interaction.Dispatch(e);
+		private async Task EditHandler(MessageUpdateEventArgs e) {
+			foreach (var intr in IMC.InteractionRegistry) {
+				await intr.BotMessages.Wants(e);
+				await intr.UserMessages.Wants(e);
+			}
 		}
 
-		public void Deactivate()
-		{
+		public void Deactivate() {
 			Name = null;
 			Description = null;
 			Status = false;
 		}
-		public void Execute()
-		{
+		public void Execute() {
 
 		}
 	}
