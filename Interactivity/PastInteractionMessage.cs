5e86b606a4cf7bbb8890758c11100f5baa85a2b8
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-05-16 18:43:00 -0300

    Adds `PastInteractionMessage` class to be used as a readonly interaction message

---
+++ Interactivity/PastInteractionMessage.cs
@@ -0,0 +1,26 @@
+﻿using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sisbase.Interactivity {
+	public class PastInteractionMessage {
+		public string Content { get; internal set; }
+		public IReadOnlyList<DiscordEmbed> Embeds { get; internal set; }
+		public IReadOnlyList<DiscordUser> MentionedUsers { get; internal set; }
+		public IReadOnlyList<DiscordRole> MentionedRoles { get; internal set; }
+		public IReadOnlyList<DiscordChannel> MentionedChannels { get; internal set; }
+		public DateTimeOffset Timestamp { get; internal set; }
+		public DateTimeOffset? EditedTimestamp { get; internal set; }
+
+		internal PastInteractionMessage(DiscordMessage m) {
+			Content = m.Content;
+			Embeds = m.Embeds;
+			MentionedChannels = m.MentionedChannels;
+			MentionedRoles = m.MentionedRoles;
+			MentionedUsers = m.MentionedUsers;
+			Timestamp = m.Timestamp;
+			EditedTimestamp = m.EditedTimestamp;
+		}
+	}
+}
