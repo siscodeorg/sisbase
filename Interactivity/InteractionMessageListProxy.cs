c2334d0773b5cb0077c4f3bdf88cc31cb869e5de
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-05-16 01:15:16 -0300

    Starts working on InteractionMessageListProxy

---
+++ Interactivity/InteractionMessageListProxy.cs
@@ -0,0 +1,32 @@
+﻿using sisbase.Interactivity.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sisbase.Interactivity
+{
+	public class InteractionMessageListProxy
+	{
+		public InteractionMessageListProxyMode Mode { get; internal set; }
+		internal Interaction Parent;
+		public InteractionMessage Get(InteractionMessageProxyMode mode)
+		{
+            if (Mode == InteractionMessageListProxyMode.BOT)
+            {
+                if (mode == InteractionMessageProxyMode.FIRST)
+                    return Parent.BotMessages.FirstOrDefault();
+                else
+                    return Parent.BotMessages.LastOrDefault();
+            }
+            else
+            {
+                if (mode == InteractionMessageProxyMode.FIRST)
+                    return Parent.UserMessages.FirstOrDefault();
+                else
+                    return Parent.UserMessages.LastOrDefault();
+            }
+
+        }
+	}
+}
