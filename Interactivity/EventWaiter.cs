36c957db312a07e873eaf426de738f9820c2dfc5
wffirilat <xwffirilat@gmail.com>
2020-06-02 16:39:46 -0500

    Make EventWaiter cancellable

--- Interactivity/EventWaiter.cs
+++ Interactivity/EventWaiter.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using sisbase.Utils;
 
 namespace sisbase.Interactivity {
     public class EventWaiter<T> {
         private readonly Func<T, bool> pred;
         private readonly TaskCompletionSource<T> taskSource = new TaskCompletionSource<T>();
+        private readonly CancellationTokenSource token;
 
         public Task<T> Task => taskSource.Task;
 
-        public EventWaiter(Func<T, bool> pred) {
+        public EventWaiter(Func<T, bool> pred, TimeSpan timeout = default, CancellationToken token = default) {
+            this.token = ConcurrencyUtils.PrepareTimeoutToken(timeout, token);
             this.pred = pred;
         }
 
         public bool Offer(T args) {
+            if (token.IsCancellationRequested) {
+                taskSource.SetCanceled();
+                return true;
+            }
             if (pred(args))
                 return taskSource.TrySetResult(args);
             return false;
