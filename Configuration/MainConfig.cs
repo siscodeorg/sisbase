02ea433b3ce658574a627a33ff2d28cc39d9f517
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-06-13 16:17:47 -0300

    Removed unused properties/methods from `MainConfig.cs`

--- Configuration/MainConfig.cs
+++ Configuration/MainConfig.cs
@@ -18,21 +18,9 @@ namespace sisbase.Configuration {
             }
             else {
                 Data = General.TUI_cfg();
-                Data.CommandSettings.Groups["config"] = new GroupSetting {
-                    Enabled = true
-                };
-                Data.CommandSettings.Groups["system"] = new GroupSetting {
-                    Enabled = true
-                };
                 Update();
             }
         }
-
-        public T GetConfig<T>(string Path) 
-            => (T) Data.CustomSettings[Path];
-
-        public void SetConfig<T>(string Path, T value)
-            => Data.CustomSettings[Path] = value;
     }
 
     public class MainConfigData {
@@ -40,18 +28,7 @@ namespace sisbase.Configuration {
         [JsonProperty] public ulong MasterId { get; set; }
         [JsonProperty] public List<ulong> PuppetId { get; set; } = new List<ulong>();
         [JsonProperty] public List<string> Prefixes { get; set; } = new List<string>();
-        [JsonProperty] public bool EnableDspLogger { get; set; }
-        [JsonProperty] public GroupCommandData CommandSettings { get; set; } = new GroupCommandData();
         [JsonProperty] internal Dictionary<string,object> CustomSettings { get; set; } = new Dictionary<string, object>();
         
-    }
-
-    public class GroupCommandData {
-        public bool EnableSisbaseHelp { get; set; } = true;
-        public Dictionary<string, GroupSetting> Groups { get; set; } = new Dictionary<string, GroupSetting>();
-    }
-    public class GroupSetting {
-        //public string Name { get; set; } TBA
-        public bool Enabled { get; set; } = true;
     }
 }
