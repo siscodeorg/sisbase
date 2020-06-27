ce881d962900cd960ff1da1f1bc72207eb081f2a
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-06-27 16:41:59 -0300

    Add AsyncLogMessageEventArgs

---
+++ EventArgs/AsyncLogMessageEventArgs.cs
@@ -0,0 +1,35 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using System;
+
+namespace sisbase.EventArgs {
+    public class AsyncLogMessageEventArgs : AsyncEventArgs {
+        //
+        // Summary:
+        //     Gets the level of the message.
+        public LogLevel Level { get; internal set; }
+        //
+        // Summary:
+        //     Gets the name of the application which generated the message.
+        public string Application { get; internal set; }
+        //
+        // Summary:
+        //     Gets the sent message.
+        public string Message { get; internal set; }
+        //
+        // Summary:
+        //     Gets the exception of the message.
+        public Exception Exception { get; internal set; }
+        //
+        // Summary:
+        //     Gets the timestamp of the message.
+        public DateTime Timestamp { get; internal set; }
+        internal AsyncLogMessageEventArgs(DebugLogMessageEventArgs dspargs) {
+            Level = dspargs.Level;
+            Application = dspargs.Application;
+            Message = dspargs.Message;
+            Exception = dspargs.Exception;
+            Timestamp = dspargs.Timestamp;
+        }
+    }
+}
