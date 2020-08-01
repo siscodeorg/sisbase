3496aab89c80212257fabd1835efaa4d2c28a72e
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-07-31 22:17:25 -0300

    Add SchemaUtils.cs

---
+++ Utils/SchemaUtils.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sisbase.Utils {
+    public static class SchemaUtils {
+        private static ConcurrentDictionary<Type, JSchema> cache { get; } = new ConcurrentDictionary<Type, JSchema>();
+        private static JSchemaGenerator gen = new JSchemaGenerator();
+        public static JSchema For<T>() {
+            if (cache.ContainsKey(typeof(T))){
+                return cache[typeof(T)];
+            }
+            cache.TryAdd(typeof(T), gen.Generate(typeof(T)));
+            return cache[typeof(T)];
+        }
+    }
+}
