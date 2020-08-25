70037c58a100223a0e2300b57e1862493885508d
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-08-24 22:58:37 -0300

    Adds the proposed hierarchy utils

---
+++ Utils/HierarchyUtils.cs
@@ -0,0 +1,10 @@
+﻿using DSharpPlus.Entities;
+using System.Linq;
+
+namespace sisbase.Utils {
+    public static class HierarchyUtils {
+        public static DiscordRole GetHighestRole(this DiscordMember m) => m.Roles.OrderByDescending(x => x.Position).FirstOrDefault();
+        public static bool IsAbove(this DiscordMember m, DiscordRole r) => m.Hierarchy > r.Position;
+        public static bool IsAbove(this DiscordRole left, DiscordRole right) => left.Position > right.Position;
+    }
+}
