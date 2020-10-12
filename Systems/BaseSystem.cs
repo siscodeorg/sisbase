3e58f79f239d2be4b4fec37e46d76b85fe1e7b51
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-10-12 12:35:32 -0300

    SMC v3 API Specification
    Implements BaseSystem and ClientSystem as Abstract Classes
    
    Note : SystemManager is not currently implemented since the api is required for its implementation.
    
    Changes from SMC v2:
    - Execute renamed to Preconditions and made optional (virtual)
    - Made ApplyToClient virtual
    - Made Activate and Deactivate asynchronous
    - SMC API's are now fully async. (Task-based)

--- Systems/BaseSystem.cs
+++ Systems/BaseSystem.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace sisbase.Systems {
     public abstract class BaseSystem {
+        public SisbaseBot SisbaseInstance;
+        public string Name;
+        public string Description;
+        public bool Status = true;
+        public abstract Task Activate();
+        public abstract Task Deactivate();
+        public virtual async Task<bool> CheckPreconditions() { return true; }
     }
 }
--- Systems/ClientSystem.cs
+++ Systems/ClientSystem.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using DSharpPlus;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace sisbase.Systems {
     public abstract class ClientSystem : BaseSystem {
+        public DiscordClient Client;
+        public virtual async Task ApplyToClient(DiscordClient client) { }
     }
 }
