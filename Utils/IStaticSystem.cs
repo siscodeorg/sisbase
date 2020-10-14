7a43f3210e4360b9494bc6b4701c2be3c7f5b8c1
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-10-14 18:58:51 -0300

    Remove files from old smc v2

--- Utils/IStaticSystem.cs
+++
@@ -1,9 +0,0 @@
-namespace sisbase.Utils
-{
-	/// <summary>
-	/// Interface for systems that don't require connection to <see cref="DSharpPlus"/>
-	/// </summary>
-	public interface IStaticSystem : ISystem
-	{
-	}
-}
