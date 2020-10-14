75f12e03a8800e884c8f96c2f4ffed31f43b5843
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-10-14 18:58:06 -0300

    Add TypeExtensions.cs

---
+++ Utils/Extensions/TypeExtensions.cs
@@ -0,0 +1,38 @@
+﻿using sisbase.Systems;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace sisbase.Utils.Extensions {
+    internal static class TypeExtensions {
+		internal static bool IsCandidate(this TypeInfo ti) {
+			// check if compiler-generated
+			if (ti.GetCustomAttribute<CompilerGeneratedAttribute>(false) != null)
+				return false;
+
+			// check if derives from the required base class
+			var tmodule = typeof(BaseSystem);
+			var timodule = tmodule.GetTypeInfo();
+			if (!timodule.IsAssignableFrom(ti))
+				return false;
+
+			// check if anonymous
+			if (ti.IsGenericType && ti.Name.Contains("AnonymousType") && (ti.Name.StartsWith("<>") || ti.Name.StartsWith("VB$")) && (ti.Attributes & TypeAttributes.NotPublic) == TypeAttributes.NotPublic)
+				return false;
+
+			// check if abstract, static, or not a class
+			if (!ti.IsClass || ti.IsAbstract)
+				return false;
+
+			// check if delegate type
+			var tdelegate = typeof(Delegate).GetTypeInfo();
+			if (tdelegate.IsAssignableFrom(ti))
+				return false;
+
+			// qualifies if any method or type qualifies
+			return true;
+		}
+	}
+}
