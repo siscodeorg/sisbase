ebf102ecd035efadd8d5f56b479e9a3985749c4f
wffirilat <xwffirilat@gmail.com>
2020-10-28 16:03:28 -0500

    Add EventWaitHandler fields to InteractivityManager

--- Systems/InteractivityManager.cs
+++ Systems/InteractivityManager.cs
@@ -14,12 +14,24 @@ namespace sisbase.Systems {
 		
 		internal readonly EventWaitHandler<DiscordEventArgs> ReactionToggleWaiter = new EventWaitHandler<DiscordEventArgs>();
 
+		public readonly EventWaitHandler<MessageUpdateEventArgs> messageUpdateWaiter = new EventWaitHandler<MessageUpdateEventArgs>();
+		public readonly EventWaitHandler<MessageDeleteEventArgs> messageDeleteWaiter = new EventWaitHandler<MessageDeleteEventArgs>();
+		public readonly EventWaitHandler<MessageReactionAddEventArgs> messageReactionAddWaiter = new EventWaitHandler<MessageReactionAddEventArgs>();
+		public readonly EventWaitHandler<MessageReactionRemoveEventArgs> messageReactionRemoveWaiter = new EventWaitHandler<MessageReactionRemoveEventArgs>();
+		public readonly EventWaitHandler<MessageCreateEventArgs> messageCreateWaiter = new EventWaitHandler<MessageCreateEventArgs>();
+
 		public void InitWaitListeners(DiscordClient client) {
 			client.MessageUpdated += EventWaiter<MessageUpdateEventArgs>.Listener;
 			client.MessageDeleted += EventWaiter<MessageDeleteEventArgs>.Listener;
 			client.MessageReactionAdded += EventWaiter<MessageReactionAddEventArgs>.Listener;
 			client.MessageReactionRemoved += EventWaiter<MessageReactionRemoveEventArgs>.Listener;
 			client.MessageCreated += EventWaiter<MessageCreateEventArgs>.Listener;
+
+			client.MessageUpdated += messageUpdateWaiter.Listener;
+			client.MessageDeleted += messageDeleteWaiter.Listener;
+			client.MessageReactionAdded += messageReactionAddWaiter.Listener;
+			client.MessageReactionRemoved += messageReactionRemoveWaiter.Listener;
+			client.MessageCreated += messageCreateWaiter.Listener;
 			client.MessageReactionRemoved += ReactionToggleWaiter.Offer;
 			client.MessageReactionAdded += ReactionToggleWaiter.Offer;
 		}
